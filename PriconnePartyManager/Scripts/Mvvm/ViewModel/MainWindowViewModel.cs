c27a386d3aa45b1215967953bc9a3e6fe83c8198
Hitsub <hitsumabushis@gmail.com>
2021-05-18 12:28:23 +0900

    凸ルートに編成したパーティが既にマイパーティから削除されている場合、確認を取るように

--- PriconnePartyManager/Scripts/Mvvm/ViewModel/MainWindowViewModel.cs
+++ PriconnePartyManager/Scripts/Mvvm/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using PriconnePartyManager.Scripts.Common;
 using PriconnePartyManager.Scripts.DataModel;
 using PriconnePartyManager.Scripts.Mvvm.Common;
@@ -88,13 +89,28 @@ namespace PriconnePartyManager.Scripts.Mvvm.ViewModel
         private void OnUnSelectAttackRoute(UserParty party)
         {
             var target = AttackParties.SingleOrDefault(x => x.Id == party.Id);
-            AttackParties.Remove(target);
             var userParty = UserParties.SingleOrDefault(x => x.Party.Value.Id == party.Id);
             //凸ルート編成後にユーザーパーティを消す場合があるので必ずチェックする
             if (userParty != null)
             {
                 userParty.IsSelectedRoute.Value = false;
             }
+            if (userParty == null)
+            {
+                var result = MessageBox.Show("マイパーティ一覧から既にこの編成が消えています。\nこの編成をマイパーティに追加して凸ルートから削除しますか？", "警告",
+                    MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Yes)
+                {
+                    var newParty = new UserParty(party.UserUnits, party.Comment, party.EstimateDamage);
+                    Database.I.AddParty(newParty);
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+                
+            }
+            AttackParties.Remove(target);
             CheckDoubling();
         }
 
@@ -126,6 +142,10 @@ namespace PriconnePartyManager.Scripts.Mvvm.ViewModel
             {
                 foreach (var userUnitViewModel in attackPartyViewModel.PartyUnits)
                 {
+                    if (userUnitViewModel.UserUnit.IsSupport)
+                    {
+                        continue;
+                    }
                     userUnitViewModel.IsDoubling.Value = doublingIds.Contains(userUnitViewModel.UserUnit.UnitId);
                 }
             }
